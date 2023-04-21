6a4f6a39c4086dc0b65caab582c493f2af0356fd
rickparra <hpbenitez7@gmail.com>
2023-04-20 21:00:49 -0300

    final da aula

--- js/script.js
+++ js/script.js
@@ -1,24 +1,58 @@
 
 let tmp = "";
 
-function mudaCor() {
+// function mudaCor() {
     
-    let r = 0;
-    let g = 0;
-    let b = 0;
+//     let r = 0;
+//     let g = 0;
+//     let b = 0;
 
-    //Math
-    r = Math.ceil(Math.random() * 255);
-    g = Math.ceil(Math.random() * 255);
-    b = Math.ceil(Math.random() * 255);
+//     //Math
+//     r = Math.ceil(Math.random() * 255);
+//     g = Math.ceil(Math.random() * 255);
+//     b = Math.ceil(Math.random() * 255);
 
-    const elementos = [...document.getElementsByClassName("cabecalho")];
+//     const elementos = [...document.getElementsByClassName("cabecalho")];
     
-    elementos.forEach((el)=>{
-        el.style.backgroundColor = `rgb(${r},${g},${b})`;
-    })
+//     elementos.forEach((el)=>{
+//         el.style.backgroundColor = `rgb(${r},${g},${b})`;
+//     })
 
-    tmp = setTimeout(mudaCor, 1000);
+//     tmp = setTimeout(mudaCor, 5000);
+// }
+mudaBanner1
+
+function mudaBanner1() {
+    
+    let banner1 = "./img/./img/banner-lateral-1.png";
+
+
+    const elImg = document.querySelector('.cabecalho > img')
+
+    elImg.src = banner1;
+
+    setTimeout(mudaBanner2, 1000)
 }
+
+function mudaBanner2() {
     
-mudaCor()
+    let banner2 = "../img/banner-lateral-2.png";
+
+    const elImg = document.querySelector('.cabecalho > img')
+
+    elImg.src = banner2;
+
+    setTimeout(mudaBanner3, 1000)
+}
+
+function mudaBanner3() {
+    
+    let banner3 = "./img/banner-lateral-3.png"
+
+    const elImg = document.querySelector('.cabecalho > img')
+
+    elImg.src = banner3;
+
+    setTimeout(mudaBanner1, 1000)
+}
+
